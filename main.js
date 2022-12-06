47da4c38fb7a8190855960557d668e379ecfa55c
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-06 18:15:34 -0300

    constructor

--- main.js
+++ main.js
@@ -5,5 +5,18 @@ const allClearButtons  = document.querySelectorAll("[data-all-clear]");
 const deleteButtons    = document.querySelectorAll("[data-delete]");
 const equalsButtons    = document.querySelectorAll("[data-equals]");
 
-const previousOperand  = document.querySelector("data-previous-operand");
-const currentOperand   = document.querySelector("data-current-operand");
+// output
+const previousOperandTextElement  = document.querySelector("data-previous-operand");
+const currentOperandTextElement   = document.querySelector("data-current-operand");
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement  = currentOperandTextElement;
+    }
+
+    clear() {
+        this.previousOperandTextElement = '';
+        this.currentOperandTextElement  = '';
+    }
+}
