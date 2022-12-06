18e38d8d62454e0bb065df3334c37b5dbd9c6279
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-06 18:12:44 -0300

    js -> selecting the buttons

---
+++ main.js
@@ -0,0 +1,9 @@
+// buttons
+const numberButtons    = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const allClearButtons  = document.querySelectorAll("[data-all-clear]");
+const deleteButtons    = document.querySelectorAll("[data-delete]");
+const equalsButtons    = document.querySelectorAll("[data-equals]");
+
+const previousOperand  = document.querySelector("data-previous-operand");
+const currentOperand   = document.querySelector("data-current-operand");
